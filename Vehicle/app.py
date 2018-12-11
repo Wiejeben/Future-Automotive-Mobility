a3d312fce83f010ac0f4ef21c0558e4709bed48e
Maarten de Graaf <wiejeben@gmail.com>
2018-12-11 12:57:47 +0100

    Add test code

--- Vehicle/app.py
+++ Vehicle/app.py
@@ -1,48 +1,12 @@
-import RPi.GPIO as GPIO
+import * from Vehicle
+import * from Motor
 
-class Vehicle:
-    def __init__(self):
-        GPIO.setmode(GPIO.BOARD)
-        self.left = Motor(0, 0, 0)
-        self.right = Motor(0, 0, 0)
+vehicle = Vehicle()
 
-    def __del__(self):
-        GPIO.cleanup()
+try:
+    while True:
+        vehicle.forward()
+except KeyboardInterrupt:
+    print('Ending program')
 
-class Motor:
-    def __init__(self, pin_forward, pin_backward, pin_pwm):
-        self.pin_forward = pin_forward
-        self.pin_backward = pin_backward
-        self.pin_pwm = pin_pwm
-        self.power = GPIO.PWM(pin_pwm, 50)
-
-        GPIO.setup(pin_forward, GPIO.OUT)
-        GPIO.setup(pin_backward, GPIO.OUT)
-        GPIO.output(pin_forward, GPIO.LOW)
-        GPIO.output(pin_backward, GPIO.LOW)
-
-    def forward(self):
-        print('Forward')
-
-        self.power.start(1)
-        GPIO.output(self.pin_forward, GPIO.HIGH)
-        GPIO.output(self.pin_forward, GPIO.LOW)
-
-    def reverse(self):
-        print('Reverse')
-
-        self.power.start(1)
-
-        GPIO.output(self.pin_backward, GPIO.HIGH)
-        GPIO.output(self.pin_backward, GPIO.LOW)
-
-    def stop(self):
-        print('Stop')
-
-        self.power.stop()
-
-    def __del__(self):
-        self.stop()
-
-if __name__ == '__main__':
-    vehicle = Vehicle()
+vehicle.exit()
