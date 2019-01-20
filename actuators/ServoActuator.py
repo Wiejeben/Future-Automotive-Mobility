8e89f814464fc36da66c9312bbaf90c8bd1f0140
Maarten de Graaf <wiejeben@gmail.com>
2019-01-20 17:11:56 +0100

    Cleanup

--- actuators/ServoActuator.py
+++ actuators/ServoActuator.py
@@ -5,30 +5,27 @@ from actuators.Actuator import Actuator
 
 
 class ServoActuator(Actuator):
-    def __init__(self, pin: int):
+    def __init__(self, pin: int, base: int = 6, power_devision: int = 100):
         GPIO.setup(pin, GPIO.OUT)
         self.power = GPIO.PWM(pin, 100)
-        self.power.start(0)
+        self.power.start(base)
 
-        self.base = 6
-        self.power_division = 100
+        self.base = base
+        self.power_division = power_devision
 
         super().__init__()
 
     def forward(self, power: int = 100):
         dc = self.base - (power / self.power_division)
-        print(dc)
         self.power.ChangeDutyCycle(dc)
         super().forward(power)
 
     def reverse(self, power: int = 100):
         dc = self.base + (power / self.power_division)
-        print(dc)
         self.power.ChangeDutyCycle(dc)
         super().reverse(power)
 
     def neutral(self):
-        print(self.base)
         self.power.ChangeDutyCycle(self.base)
         super().neutral()
 
